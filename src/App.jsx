2e6a4f60f6530a74436069023b0cea1482f6b6dd
Jared <jaredterrospin@gmail.com>
2023-03-28 19:18:33 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./compnents/Navbar"
+import Main from "./compnents/Main"
+
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
